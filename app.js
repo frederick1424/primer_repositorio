f109b3b7a55a63f41c4d82721c5e970de807209d
frederick <fredefickdelacruz123@gmail.com>
2020-05-17 10:46:41 -0400

    agregar app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let passwordDB = 'pepe123'
+let input = 'pepe1233'
+
+let resul = input == passwordDB;
+if(resul == true) 
+{
+    console.log('login correcto');
+
+}
+if(resul== false){
+    console.log('login incorrecto');
+}
+console.log(resul)
